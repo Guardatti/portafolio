d899d4fc6a8d399fe3de6ffc071bcdd55cb5f832
Nicolas <nicolasguardattiutn@gmail.com>
2025-11-07 11:22:04 -0300

    .

---
+++ src/components/Form.tsx
@@ -0,0 +1,97 @@
+import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast, ToastContainer } from 'react-toastify';
+import emailjs from '@emailjs/browser'
+import '../styles/form.css'
+
+interface FormData {
+    user_name: string,
+    user_email: string,
+    message: string,
+}
+
+
+const Form: React.FC = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const form = useRef<HTMLFormElement>(null);
+
+    const onSubmit = () => {
+
+        setLoading(true);
+
+        if (!form.current) return;
+
+        emailjs
+        .sendForm('service_9c9qnia', 'template_n7mvs14', form.current, {
+            publicKey: '4J18PYcgaGGgyLwvL',
+        })
+        .then(
+            () => {
+            toast.success('Mensaje enviado con éxito', {
+            position: "bottom-center",
+            autoClose: 2000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+            });
+            form.current?.reset();
+            setLoading(false);
+            },
+            () => {
+            toast.error('Error al enviar el mensaje', {
+            position: "bottom-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+            });
+            setLoading(false);
+            },
+        );
+    };
+    
+    return (
+        <>
+            <form ref={form} onSubmit={handleSubmit(onSubmit)} className='container-contact-form'>
+                <div className='container-input-contact-form'>
+                    <input type="text" {...register('user_name', {required: true})} id="nombre" placeholder='Nombre'/>
+                    {errors.user_name && <span style={{fontSize: '12px', color: 'red', width: '80%'}}>Campo obligatorio</span>}
+                </div>
+                <div className='container-input-contact-form'>
+                    <input type="email" {...register("user_email", {required: "Campo obligatorio", pattern: {value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: "El email no es válido"}})} placeholder="Email" />
+                    {errors.user_email && <span style={{fontSize: '12px', color: 'red', width: '80%'}}>{errors.user_email.message}</span>}
+                </div>
+                <div className='container-input-contact-form'>
+                    <textarea className='textarea-contact-form' {...register('message', {required: true})} placeholder='Escribe tu mensaje aquí'></textarea>
+                    {errors.message && <span style={{fontSize: '12px', color: 'red', width: '80%'}}>Campo obligatorio</span>}
+                </div>
+                <div className='container-input-contact-form'>
+                    {
+                    loading ? (
+                        <button className='loading-contact-form' disabled={true}>
+                            <div className='spinner-contact-form'/>
+                        </button>
+                    )
+                    :
+                    (
+                        <button type="submit" className="loading-contact-form">Enviar</button>
+                    )
+                    }
+                </div>
+            </form>
+            <ToastContainer />
+        </>
+    )
+}
+
+export default Form
